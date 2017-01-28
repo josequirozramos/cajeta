3d9a6ba554d8edeaf79cbb06ae478efe8917e7eb
jquiroz <jquiroz@jquiroz-l2.dart.corp>
2017-01-27 17:23:11 -0800

    Initial commit of Beer song

---
+++ LoopyProject/src/Loopy.java
@@ -0,0 +1,14 @@
+
+public class Loopy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =1;
+		System.out.println("Before the loop");
+		while (x<40) {
+			System.out.println("In the loop");
+			System.out.println("Value of X is " + x);
+			x=x+1;
+		}
+	}
+}
